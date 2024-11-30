a1556f2d3bef0a94981a399db44c8acf42bf80e3
胆小鬼爱丽丝 <1056781722@qq.com>
2024-11-30 21:05:21 +0800

    2024/11/30

--- oat-llvm40/lib/Transforms/CFVHints/CollectCondBranchHints.cpp
+++ oat-llvm40/lib/Transforms/CFVHints/CollectCondBranchHints.cpp
@@ -30,22 +30,21 @@
 
 #define DEBUG_TYPE "collect-cond-branch-hints"
 
-// The MACROs defined below only for evaluation, controlling whether we instrument all function or not
+// “下面定义的宏仅用于评估，控制是否对所有函数进行插桩。”
 #define INSTRUMENT_ALL
 
-STATISTIC(NumOfCondBranches, "Number of conditional branch inst");
+STATISTIC(NumOfCondBranches, "Number of conditional branch inst");//统计 被插桩的条件分支指令的数量，并在最终的编译输出中显示这个统计信息
 
 using namespace llvm;
 
 typedef std::map<std::string, int> FunctionMap;
 
-// Make it configurable whether or not to instrument the conditional branch.
+//用于控制是否收集条件分支提示。默认值是 true，可以通过命令行设置为 false 来禁用此功能。
 static cl::opt<bool> CollectCondBranchHintsInfo("collect-cond-branch-hints", cl::Hidden,
                                   cl::init(true));
-
+//指定一个文件，文件中列出了需要插桩的函数名称
 static cl::opt<std::string> InstrumentFunctionNameListFile("funclist", cl::Hidden, cl::desc("Specify input file that list the function names that need to be instrumented"),
                                   cl::init("funclist.txt"));
-
 namespace {
 struct  CollectCondBranchHints : public FunctionPass {
   static char ID;
@@ -54,12 +53,13 @@ struct  CollectCondBranchHints : public FunctionPass {
   static std::vector<std::string> *list;
 
   bool Modified = false;
-
+//构造函数：初始化了一个 FunctionPass 类型的基类，并且传递了一个 ID 参数
   CollectCondBranchHints() : FunctionPass(ID) {
     list->clear();
 
-    std::ifstream infile(InstrumentFunctionNameListFile);
+    std::ifstream infile(InstrumentFunctionNameListFile);//通过命令行参数传递的 -funclist 选项指定的文件路径
     std::string word;
+    //读取文件内容:函数名称
     while(infile>>word) {
         list->push_back(word);
     }
@@ -67,34 +67,34 @@ struct  CollectCondBranchHints : public FunctionPass {
         errs() << "Note: funclist is empty! use -funclist filename to designate funclist file" << "\n";
     }
   };
-
+    //插桩核心代码：
   bool runOnFunction(Function &F) override {
-    std::string name = F.getName().str();
+    std::string name = F.getName().str();//获取函数名字
 
-#ifdef INSTRUMENT_ALL // instrument all
-#else // instrument selected functions
+#ifdef INSTRUMENT_ALL // 如果宏 INSTRUMENT_ALL 被定义，则会对所有函数进行插桩
+#else // 将根据一个外部提供的函数名列表（InstrumentFunctionNameListFile 参数指定的文件）决定是否对当前函数进行插桩。
     errs() << __func__ << " : "<< name << "\n";
     if (std::find(std::begin(*list), std::end(*list), name) == std::end(*list)) {
+        //如果函数名称 name 不在 list 中（即函数不在需要插桩的函数列表里），则跳过该函数，输出信息并返回 false。
         errs() << __func__ << ": skip function "<< name << ", for it is not in list"<<"\n";
         return false;
     }
 #endif
-
-    // check whether we have visit this function before
+    // 检查我们是否之前访问过这个函数
     if (fMap->find(name) != fMap->end()) {
 	(*fMap)[name] = ++FID; 
     }
-
+    //遍历函数中的每个基本块
     for (BasicBlock &BB : F) {
-        for  (Instruction &I : BB) {
-            switch (I.getOpcode()) {
-                case Instruction::Br: {
+        for  (Instruction &I : BB) {//每一个指令
+            switch (I.getOpcode()) {//根据指令的操作码（opcode）来判断指令的类型
+                case Instruction::Br: {//分支指令
                     BranchInst *bi = cast<BranchInst>(&I);
-                    if (bi->isUnconditional()) {
+                    if (bi->isUnconditional()) {//无条件分支
                         //errs() << *bi << "\n";
-                    } else {
+                    } else {//有条件分支
                         //errs() <<"CondBranch: " << *bi << "\n";
-                        Modified |= instrumentCondBranch(&I, bi->getCondition());
+                        Modified |= instrumentCondBranch(&I, bi->getCondition());//插桩
                         NumOfCondBranches++;
                     }
                     break;
@@ -110,13 +110,13 @@ struct  CollectCondBranchHints : public FunctionPass {
     return Modified;
   }
 
-  bool instrumentCondBranch(Instruction *I, Value *cond);
+  bool instrumentCondBranch(Instruction *I, Value *cond);//插桩代码
 
 };
 } // end of namespace
 
 bool CollectCondBranchHints::instrumentCondBranch(Instruction *I, Value *cond) {
-    IRBuilder<> B(I);
+    IRBuilder<> B(I);//IRBuilder 会在指令 I 后面插入新的 IR 指令
     Module *M = B.GetInsertBlock()->getModule();
     Type *VoidTy = B.getVoidTy();
     Type *I1Ty = B.getInt1Ty();
@@ -125,11 +125,11 @@ bool CollectCondBranchHints::instrumentCondBranch(Instruction *I, Value *cond) {
 
     Constant *ConstCollectCondBranchHints= M->getOrInsertFunction("__collect_cond_branch_hints", VoidTy,
                                                                      I1Ty, 
-                                                                     nullptr);
+                                                                     nullptr);//插入函数声明
 
     Function *FuncCollectCondBranchHints= cast<Function>(ConstCollectCondBranchHints);
 
-    B.CreateCall(FuncCollectCondBranchHints, {cond});
+    B.CreateCall(FuncCollectCondBranchHints, {cond});//在条件分支处插入函数调用
 
     return true;
 
